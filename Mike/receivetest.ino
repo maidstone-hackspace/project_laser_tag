6dea3dc7412259e17d79da2f7f5e73b6237a9bcc
Mike McRoberts <thearduinoguy@users.noreply.github.com>
2016-08-30 16:26:14 +0100

    Update receivetest.ino

--- Mike/receivetest.ino
+++ Mike/receivetest.ino
@@ -5,6 +5,7 @@
    Copyright 2009 Ken Shirriff
    http://arcfn.com
 */
+uint8_t damageTable[]= {1,2,4,5,7,10,15,17,20,25,30,35,40,50,75,100};
 
 #include <IRremote.h>
 
@@ -16,7 +17,7 @@ decode_results results;
 
 void setup()
 {
-  Serial.begin(9600);
+  Serial.begin(115200);
   delay(1000);
   irrecv.enableIRIn(); // Start the receiver
 }
@@ -27,13 +28,12 @@ void printData(uint16_t TEAM, uint16_t PLAYER, uint16_t DAMAGE) {
    Serial.print(" from Team ");
     Serial.print(TEAM);
     Serial.print(" for ");
-    Serial.print(DAMAGE);
+    Serial.print(damageTable[DAMAGE]);
     Serial.println(" Damage.");
 }
 
 void loop() {
   if (irrecv.decode(&results)) {
-    Serial.println(results.value, BIN);
     uint16_t TEAM = (results.value & (((1 << 2) - 1) << 9)) >> 9;
     uint16_t PLAYER = (results.value & (((1 << 5) - 1) << 4)) >> 4;
     uint16_t DAMAGE = (results.value & (((1 << 4) - 1)));
