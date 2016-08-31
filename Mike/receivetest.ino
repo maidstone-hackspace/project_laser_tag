d14ccde6f2aa5d3e86518c77e83d05966a5c714d
Mike McRoberts <thearduinoguy@users.noreply.github.com>
2016-08-31 17:08:25 +0100

    Added code to validate the data using a 16 bit CRC

--- Mike/receivetest.ino
+++ Mike/receivetest.ino
@@ -1,10 +1,7 @@
-/*
-   IRremote: IRrecvDemo - demonstrates receiving IR codes with IRrecv
-   An IR detector/demodulator must be connected to the input RECV_PIN.
-   Version 0.1 July, 2009
-   Copyright 2009 Ken Shirriff
-   http://arcfn.com
-*/
+#include <FastCRC.h>
+
+FastCRC16 CRC16;
+
 uint8_t damageTable[]= {1,2,4,5,7,10,15,17,20,25,30,35,40,50,75,100};
 char* teamTable[]= {"RED", "BLUE", "YELLOW", "GREEN"};
 
@@ -18,7 +15,7 @@ decode_results results;
 
 void setup()
 {
-  Serial.begin(115200);
+  Serial.begin(115200); 
   delay(1000);
   irrecv.enableIRIn(); // Start the receiver
 }
@@ -31,16 +28,23 @@ void printData(uint16_t TEAM, uint16_t PLAYER, uint16_t DAMAGE) {
     Serial.print(" team for ");
     Serial.print(damageTable[DAMAGE]);
     Serial.println(" Damage.");
+
 }
 
 void loop() {
   if (irrecv.decode(&results)) {
+     // results.value & (((1 << NUMBER_OF_BITS) - 1) << POSITION_OF_LEFTSHIFTED_FIRST_BIT)) >> SHIFT_TO_RIGHT_EDGE
     uint16_t TEAM = (results.value & (((1 << 2) - 1) << 9)) >> 9;
     uint16_t PLAYER = (results.value & (((1 << 5) - 1) << 4)) >> 4;
     uint16_t DAMAGE = (results.value & (((1 << 4) - 1)));
-    uint8_t CHECKSUM = (results.value & (((1 << 5) - 1) << 11)) >> 11;
-    uint8_t validChecksum = (uint8_t)~(TEAM+PLAYER+DAMAGE)>>3;
-    if (validChecksum == CHECKSUM) printData(TEAM, PLAYER, DAMAGE);
+    uint16_t CRC = results.value >> 16;
+    uint16_t DATA = (TEAM<<9) | (PLAYER<<4) | DAMAGE;
+    uint8_t MSB = DATA  >> 8;
+    uint8_t LSB = (DATA << 8) >> 8;
+    uint8_t crcBuffer[2] = {MSB, LSB};
+    uint16_t validCRC = CRC16.ccitt(crcBuffer, sizeof(crcBuffer));
+    if (validCRC == CRC) printData(TEAM, PLAYER, DAMAGE);
     irrecv.resume(); // Receive the next value
   }
 }
+
